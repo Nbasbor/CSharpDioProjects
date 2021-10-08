d20315cc7b106e40f4b6dae717e8c0b57332b98c
Basbor <nanbasbor@gmail.com>
2021-10-08 20:43:20 -0300

    Commitando projeto

---
+++ dotnet-series/Enum/Genero.cs
@@ -0,0 +1,18 @@
+﻿namespace dotnet_series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Faroeste = 6,
+        Fantasia = 7,
+        FiccaoCientifica = 8,
+        Historico = 9,
+        Romance = 10,
+        Suspense = 11,
+        Terror = 12
+    }
+}
