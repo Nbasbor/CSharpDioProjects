ed750bcc4f15f91aba9ddd0a4a2cc35801bdfcf3
Basbor <nanbasbor@gmail.com>
2021-10-12 19:12:37 -0300

    Criando FilmesController e métodos Http

---
+++ Catalogo-Filmes/ApiCatalogoFilmes/ApiCatalogoFilmes/Controllers/V1/FilmesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoFilmes.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class FilmesController : ControllerBase
+    {
+        //Indica que é um método get
+        [HttpGet]
+
+        //função assincrona
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+
+
+        //Indica que é um método get que retorna um filme com o parametro idfilme tipo guild que estara na rota
+        [HttpGet("{idFilme:guid}")]
+
+        //função assincrona que retorna apenas um filme
+        public async Task<ActionResult<object>> Obter(Guid idFilme)
+        {
+            return Ok();
+        }
+
+        // Metodo Post
+        [HttpPost]
+
+           //Inserir Filme
+        public async Task<ActionResult<object>> InserirFilme(object filme)
+        {   
+            return Ok();
+        }
+
+        //HttpPut Atualiza recurso inteiro
+        [HttpPut("{idFilme:guid}")]
+
+        public async Task<ActionResult> AtualizarFilmes(Guid idFilme, object filmes)
+        {
+            return Ok();
+        }
+
+
+        //HttpPatch atualiza uma coisa em especifica
+        [HttpPatch("{idFilme:guid}/preco/{preco:double}")]
+
+        public async Task<ActionResult> AtualizarFilmes(Guid idFilme, double preco)
+        {
+            return Ok();
+        }
+
+        //Metodo Apagar
+        [HttpDelete("{idFilme:guid}")]
+
+        public async Task<ActionResult> ApagarFilme(Guid idFilme)
+        {
+            return Ok();
+        }
+    }
+}
