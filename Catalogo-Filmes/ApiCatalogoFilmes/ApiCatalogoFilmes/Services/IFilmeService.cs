d637dabf2804ca38aaf63d3b9d5210b6d4dafe4e
Basbor <nanbasbor@gmail.com>
2021-10-12 19:37:11 -0300

    Criando o Services Interface filme

---
+++ Catalogo-Filmes/ApiCatalogoFilmes/ApiCatalogoFilmes/Services/IFilmeService.cs
@@ -0,0 +1,25 @@
+﻿using ApiCatalogoFilmes.InputModel;
+using ApiCatalogoFilmes.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoFilmes.Services
+{
+    public interface IFilmeService
+    {
+
+        Task<List<FilmeViewModel>> Obter(int pagina, int quantidade);
+
+        Task<FilmeViewModel> Obter(Guid id);
+
+        Task<FilmeViewModel> Inserir(FilmeInputModel filme);
+
+        Task Atualizar(Guid id, FilmeInputModel filme);
+
+        Task Atualizar(Guid id, double preco);
+
+        Task Remover(Guid id);
+    }
+}
